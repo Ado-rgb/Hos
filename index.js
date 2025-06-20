c306840df6a97670647e9eaae193a7be0c42a61c
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-19 21:56:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ const io = socketIO(server, {
 
 //Configuraciones por seguridad
 const SESSIONS_DIR = process.env.SESSIONS_DIR || "sessions/";
+const USERS_FILE = process.env.USERS_FILE || "users.json";
 
 // Almacén de sitios hospedados
 let hostedSites = new Map(); // { siteName: { userDir, watcher, sockets } }
