da6a0b2a77a8924b489cd62336f34acc53247976
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-07-14 22:54:00 -0500

    Update contenedor.cjs

--- contenedor.cjs
+++ contenedor.cjs
@@ -1,12 +1,22 @@
+#!/usr/bin/env node
+
 const { createContainer } = require('@soymaycol/maycontainers');
 
 (async () => {
-  const username = process.argv[2] || "defaultUser";
+  const username = process.argv[2] || 'default';
 
   const container = createContainer({
     name: username,
-    distro: 'alpine'
+    distro: 'alpine',
+    shell: '/bin/sh', // o /bin/bash si prefieres
+    enableRoot: true // opcional
   });
 
-  await container.shell();
+  // Inicializa el contenedor (esto descarga y prepara todo)
+  await container.init();
+
+  // Ejecuta shell directamente con control de stdin/out
+  await container.exec('/bin/sh', {
+    stdio: 'inherit'
+  });
 })();
