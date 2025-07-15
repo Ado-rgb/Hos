19196282ad3c229c785863d2fba06713a8d9990a
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-07-14 23:04:19 -0500

    Update contenedor.cjs

--- contenedor.cjs
+++ contenedor.cjs
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const { createContainer } = require('@soymaycol/maycontainers');
+const { spawn } = require('child_process');
+const path = require('path');
 
 (async () => {
   const username = process.argv[2] || 'default';
@@ -12,11 +14,24 @@ const { createContainer } = require('@soymaycol/maycontainers');
     shell: '/bin/sh',
   });
 
-  // Prepara el contenedor
-  await container.init();
+  await container.init(); // prepara el rootfs
 
-  // Ejecuta el shell manualmente sin usar shell()
-  await container.exec('/bin/sh', {
+  const prootBin = require.resolve('@soymaycol/maycontainers/../bin/proot');
+  const rootfsPath = container.rootfsPath;
+
+  // Ejecutamos PRoot directamente, sin usar exec()
+  const pty = spawn(prootBin, [
+    '-r', rootfsPath,
+    '-w', '/root',
+    '-b', '/proc',
+    '-b', '/sys',
+    '-b', '/dev',
+    '/bin/sh' // ← comando dentro del contenedor
+  ], {
     stdio: 'inherit'
+  });
+
+  pty.on('exit', code => {
+    process.exit(code);
   });
 })();
